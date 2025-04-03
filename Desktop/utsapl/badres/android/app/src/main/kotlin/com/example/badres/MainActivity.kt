b8d21411d9dad5eef2b61a470559d61c051a51b3
AnaknyaOmDwi <rbyas2234@gmail.com>
2025-04-03 19:14:56 +0700

    Your commit message here

---
+++ Desktop/utsapl/badres/android/app/src/main/kotlin/com/example/badres/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.badres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Desktop/utsapl/projectuts/lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.green,
+          ),
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, '/quiz');
+          },
+          child: const Text('Start Quiz'),
+        ),
+      ),
+    );
+  }
+}
