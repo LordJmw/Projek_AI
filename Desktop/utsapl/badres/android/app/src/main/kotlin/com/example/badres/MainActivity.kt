983be641e7065c5c969e7bfbb573a9fe43d6ff5f
AbdRobi <gtroby2@gmail.com>
2025-04-16 11:23:23 +0700

    Delete Desktop/utsapl directory

--- Desktop/utsapl/badres/android/app/src/main/kotlin/com/example/badres/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.badres
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Desktop/utsapl/projectuts/android/app/src/main/kotlin/com/example/projectuts/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.projectuts
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
